8752cc6326e143a409e777aba0280b7274e90a64
Jordan Couture-Lafranchise <6216948@cegepmontpetit.ca>
2023-08-09 22:28:47 -0400

    Vue et action Create, EnfantVM
    ajout de la vue et de l'Action Create ainsi que la creation de l'EnfantVM

--- tp3JordanCoutureLafranchise/tp2JordanCoutureLafranchise/Controllers/GestionEnfantController .cs
+++ tp3JordanCoutureLafranchise/tp2JordanCoutureLafranchise/Controllers/GestionEnfantController .cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using tp2JordanCoutureLafranchise.Models;
+using tp2JordanCoutureLafranchise.ViewModels;
 using tp2JordanCoutureLafranchise.Models.Data;
 
 namespace tp2JordanCoutureLafranchise.Controllers
@@ -38,6 +40,47 @@ namespace tp2JordanCoutureLafranchise.Controllers
                 return RedirectToAction("Index", "Home");
             }
             return View(enfantasuprimmer);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["titre"] = "Ajouter une équipe";
+            var enfantVM = new EnfantVM
+            {
+                Enfant = new Enfant() // Create a new Enfant instance with default values
+            };
+            enfantVM.ParentSelectList = _BaseDonnees.Parents.Select(t => new SelectListItem
+            {
+                Text = t.Nom,
+                Value = t.ParentId.ToString()
+            }).OrderBy(t => t.Text);
+
+            return View(enfantVM);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(EnfantVM enfantVM)
+        {
+            if (ModelState.IsValid)
+            {
+                enfantVM.Enfant.Equipe = _BaseDonnees.Parents.FirstOrDefault(p => p.ParentId == enfantVM.Enfant.ParentId)?.Nom;
+
+                ViewData["titre"] = "Ajouter un joueur";
+                _BaseDonnees.Add(enfantVM.Enfant);
+                _BaseDonnees.SaveChanges();
+                return RedirectToAction("index", "Home");
+            }
+            enfantVM.ParentSelectList = _BaseDonnees.Parents.Select(t => new SelectListItem
+            {
+                Text = t.Nom,
+                Value = t.ParentId.ToString()
+            }).OrderBy(t => t.Text);
+
+
+            return View(enfantVM);
+
         }
     }
 }
